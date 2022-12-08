ed83fffa233d14a8e8f0a4182b847f6d4abe94f5
Mur-tal <muritala.gbadamosi@yahoo.com>
2022-12-08 07:07:10 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *  main - Print a string out using puts
+ *  Return 0 if successful, non-zero otherwise
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle.\n");
+	return (0);
+}
