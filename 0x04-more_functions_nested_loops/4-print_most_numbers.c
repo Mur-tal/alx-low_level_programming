cab2b36302460a615afe28e4e9f6e051c743cc00
Mur-tal <muritala.gbadamosi@yahoo.com>
2022-12-15 17:16:37 -0800

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function to print numbers from 0 to 9
+ * except numbers 2 and 4
+ *
+ * Return: none
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		if (i != 2 && i != 4)
+			_putchar('0' + i);
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/test_folder/4-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+int main(void)
+{
+	print_most_numbers();
+	return (0);
+}
