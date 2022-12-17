4fd9447b53b9423bf2ecacd7856ffe8fbbbbd2b7
Mur-tal <muritala.gbadamosi@yahoo.com>
2022-12-17 04:43:13 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - function to print a straight line n number of times
+ * end with a new line
+ * if n = 0 or less than 0, it should print only '\n'
+ *
+ * Return: nothing
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
