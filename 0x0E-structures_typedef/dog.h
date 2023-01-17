abbf6eb44d14418f8e664626be42bc27bd502dc4
Mur-tal <muritala.gbadamosi@yahoo.com>
2023-01-17 14:14:34 -0800

    README.md

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - a struct for a dog with it name, age and owner
+ *
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
