b5d0782d5ac2ddae48dd96156d74281424811f1d
Mur-tal <muritala.gbadamosi@yahoo.com>
2023-01-19 11:09:04 -0800

    3-print_all.c

---
+++ 0x10-variadic_functions/testFolder/3-main.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_all("ceis", 'B', 3, "stSchool");
+	print_all("ceifs", 'A', 78, 0.5, "bulawayo lagos");
+	return (0);
+}
