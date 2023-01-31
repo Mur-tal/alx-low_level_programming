a2207f336346a72eac9cdf5c89f7ea988ad4dbcd
Mur-tal <muritala.gbadamosi@yahoo.com>
2023-01-31 02:19:38 -0800

    mandatory tasks - betty compliance correctin

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -19,5 +19,6 @@ int sum_listint(listint_t *head)
 }
 /*
  * compiled with:
- * gcc -Wall -pedantic -Werror -Wextra -std=gnu89 8-main.c 3-add_nodeint_end.c 5-free_listint2.c 8-sum_listint.c
+ * gcc -Wall -pedantic -Werror -Wextra -std=gnu89 8-main.c 3-add_nodeint_end.c
+ * 5-free_listint2.c 8-sum_listint.c
  */
