0ec3a6003bdb7cc4558ad83e1cb8eb97e2820660
Mur-tal <muritala.gbadamosi@yahoo.com>
2023-01-31 13:03:47 -0800

    advance tasks

---
+++ 0x13-more_singly_linked_lists/testFolder/100-main.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    listint_t *head;
+
+    head = NULL;
+    add_nodeint_end(&head, 0);
+    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 2);
+    add_nodeint_end(&head, 3);
+    add_nodeint_end(&head, 4);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 402);
+    add_nodeint_end(&head, 1024);
+    print_listint(head);
+    reverse_listint(&head);
+    print_listint(head);    
+    free_listint2(&head);
+    return (0);
+}
+/*
+ * compile with the following files:
+ * 100-main.c 3-add_nodeint_end.c 0-print_listint.c
+ * 5-free_listint2.c 100-reverse_listint.c
+ *
+ * and run with valgrind
+ */
