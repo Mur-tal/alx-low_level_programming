ba803b66eb9fb7499d0ac5736110a45b3ac3ab39
Mur-tal <muritala.gbadamosi@yahoo.com>
2023-03-21 01:22:58 -0700

    corrected betty's complain

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -P /tmp https://github.com/mur-tal/alx-low_level_programming/blob/master\
+wget -P /tmp https://github.com/Mur-tal/alx-low_level_programming/blob/master\
 	/0x18-dynamic_libraries/101-make_me_win.sh
 export LD_PRELOAD=/tmp/libmask.so
